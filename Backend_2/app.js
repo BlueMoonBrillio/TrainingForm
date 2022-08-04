3afeee1ca00941357bc5846e49e9494704988f72
tanveerahmed20 <tanveer.ahmed@brillio.com>
2022-08-04 19:22:29 +0530

    added Joi schema validations

--- Backend_2/app.js
+++ Backend_2/app.js
@@ -1,2 +1,17 @@
 // PLEASE FIX this part , donot use boiler plate 
 
+require('./routes/server');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// middlewares 
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(PORT,()=>{
+console.log('listening on port '+PORT);
+});
--- Backend_2/models/TrainingSchema.js
+++ Backend_2/models/TrainingSchema.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-const validator  = require('validator');
-// TASK Schema: 
-const Training = mongoose.model ('training',{
+const Joi = require('joi');
+
+// Training Schema: 
+
+const schema = new mongoose.Schema({
     _id:{
         type:Number,
         required:true,
@@ -26,15 +28,29 @@ const Training = mongoose.model ('training',{
     trainingStartDate:{
         type:Date,
         required:true,
+        default: Date.now,
         trim:true,
     },
     trainingEndDate:{
         type:Date,
+        default : Date.now,
         required:true,
         trim:true,
     }
-
-
 });
+const Training = mongoose.model ('training',schema);
 
-module.exports  = Training;
+function validateTraining(training){
+    const schema = {
+        _id: Joi.number().integer().positive().required(),
+        trainingId:  Joi.number().integer().positive().required(),
+        trainerId : Joi.number().integer().positive().required(),
+        traineeId: Joi.number().integer().positive().required(),
+        trainingStartDate: Joi.date().required(),
+        trainingEndDate: Joi.date().required()
+    }
+    return Joi.validate(training,schema);
+
+}
+exports.Training  = Training;
+exports.validate = validateTraining
