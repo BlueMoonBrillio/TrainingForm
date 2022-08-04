308f9a7c1b9b29218de0d22a7069d59da52b4274
tanveerahmed20 <tanveer.ahmed@brillio.com>
2022-08-04 19:47:52 +0530

    created db folder , shifted connection.js to db folder , wrote basic app.js

--- Backend_2/app.js
+++ Backend_2/app.js
@@ -1,17 +1,21 @@
 // PLEASE FIX this part , donot use boiler plate 
 
-require('./routes/server');
+
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const trainings = require('./routes/trainings');
 
+//mongodb database connection
+require('./db/connection');
 
 // middlewares 
 app.use(cors());
 app.use(express.json());
 
 
+app.use('/trainings',trainings);
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT,()=>{
 console.log('listening on port '+PORT);
 });
