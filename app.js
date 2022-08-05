e5507977ee9fbb98fb5f9090578c1e0b951f0a5d
TanveerAhmed20 <108257503+TanveerAhmed20@users.noreply.github.com>
2022-08-05 15:44:54 +0530

    added Backend Folder

---
+++ app.js
@@ -0,0 +1,21 @@
+// PLEASE FIX this part , donot use boiler plate 
+
+const express = require('express');
+const app = express();
+const trainings = require('./routes/trainings');
+const cors=require('cors')
+
+//mongodb database connection
+require('./db/connection')();
+
+// middlewares 
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/trainings',trainings);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,()=>{
+console.log('listening on port '+PORT);
+});
---
+++ routes/trainings.js
@@ -0,0 +1,88 @@
+//  PRANAV
+const {Training,Validate} = require('../models/TrainingSchema')
+const express= require('express')
+const app=express()
+const router = express.Router()
+
+router.get('/',async (req,res)=>{
+    let results={}
+    if(!req.query.id){
+        try{
+        results = await  Training.find({}).sort('trainingId')
+        }
+        catch(err){console.log(err);  } 
+    }
+    else{
+        const id=req.query.id
+        console.log(id)
+        try{
+        results = await Training.find({'trainingId':id})
+        console.log(results)
+        }
+        catch(err){console.log(err);  } 
+    }
+    res.send(results)
+})
+
+router.get('/:id',async (req,res)=>{
+    let results={}
+        const id=req.params.id
+        console.log(id)
+        try{
+        results = await Training.find({'trainingId':id})
+        console.log(results)
+        }
+        catch(err){console.log(err);  } 
+    res.send(results)
+})
+
+router.post('/',async (req,res)=>{
+    Training.insertMany([{'trainingId': req.body.trainingId, 'trainerId': req.body.trainerId,'traineeId': req.body.traineeId,'trainingStartDate': req.body.trainingStartDate, 'trainingEndDate': req.body.trainingEndDate}])
+    .then(function(){
+        console.log("Data inserted at time:"+new Date() )  // Success
+        res.sendStatus(200)
+    }).catch(function(error){
+        console.log("Could not insert data"+error)      // Failure
+        res.status(400).send('Bad Request')
+    });
+})
+
+router.delete('/:id',async (req,res)=>{
+    const id =req.params.id;
+    console.log(id)
+    await Training.deleteOne({ trainingId: id })
+    .then(function(){
+        console.log("Data deleted at time:"+new Date() )  // Success
+        res.sendStatus(200)
+    }).catch(function(error){
+        console.log("Could not delete data"+error)      // Failure
+        res.status(400).send('Bad Request')
+    });
+})
+
+router.patch('/:id',async (req,res)=>{
+    const id =req.params.id;
+    await Training.updateOne({'trainingId':id},
+    {$set:{
+        'trainingId': req.body.trainingId , 
+        'trainerId':req.body.trainerId ,
+        'traineeId': req.body.traineeId , 
+        'trainingStartDate': req.body.trainingStartDate ,  
+        'trainingEndDate': req.body.trainingEndDate, 
+    }})
+    .then(function(){
+        console.log("Data updated at time:"+new Date() )  // Success
+        res.sendStatus(200)
+    }).catch(function(error){
+        console.log("Could not insert data"+error)      // Failure
+        res.status(400).send('Bad Request')
+    });
+})
+
+
+
+module.exports=router
+
+
+
+
